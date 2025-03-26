b0261556403795ba8ffa3d5a9bb62a9ca4be8ad4
milo <106563068+charcoalyy@users.noreply.github.com>
2025-03-26 16:27:00 -0400

    translate dcd

--- backend/src/main/kotlin/service/ResponseService.kt
+++ backend/src/main/kotlin/service/ResponseService.kt
@@ -1,15 +1,42 @@
 package com.g5.service
 
 import com.g5.model.ExpertInterface
+import com.g5.model.Prompt
 
 class ResponseService {
     private val experts: MutableList<ExpertInterface> = mutableListOf()
+    private var input: String
 
     fun addExpert(expert: ExpertInterface) {
         experts.add(expert)
     }
 
+    fun submitPrompt(prompt: Prompt) {
+        // TODO: format prompt object into string, then set that to the input attribute
+
+        input = "Not implemented yet"
+    }
+
     suspend fun generateResponse(input: String): String? {
-        return "Not identified"
+        val expertRes: MutableList<String> = mutableListOf()
+
+        for (expert in experts) {
+            val res: String = useExpert(expert, input)
+            if (res != null) expertRes.add(res)
+        }
+
+        val finalRes: String = mergeResponses(expertRes)
+
+        // TODO: find a way to determine if this is a satisfactory response, and if not, update expert informations using their answers and repeat
+
+        return finalRes
+    }
+
+    private fun useExpert(expert: ExpertInterface, input: String): String? {
+        return expert.generateResponse(input)
+    }
+
+    private fun mergeResponses(val responses: List<String>): String {
+        return "Not implemented yet"
     }
 }
