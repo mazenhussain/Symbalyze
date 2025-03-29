7f7279c4a5b07f49feaba8e7301a34887873c936
Andrew-Habib <andrew9ca@gmail.com>
2025-03-29 14:27:59 -0400

    Firebase setup + Keyword Expert DB Fill

---
+++ backend/src/main/kotlin/model/KeywordExpert.kt
@@ -0,0 +1,13 @@
+package com.g5.model
+
+abstract class KeywordExpert : ExpertInterface {
+
+    override suspend fun updateKnowledge(input: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun generateResponse(): String? {
+        TODO("Not yet implemented")
+    }
+
+}
--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
