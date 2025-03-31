e6ef9480f7f8ff840561623a1d4865d82f1d08df
milo <106563068+charcoalyy@users.noreply.github.com>
2025-03-31 17:27:37 -0400

    public api route!

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1"
 
 application {
     mainClass = "io.ktor.server.netty.EngineMain"
-    mainClass.set("com.g5.fbscripts.UploadSymbolsKt")
+    
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -22,10 +22,14 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
     testImplementation("io.mockk:mockk:1.13.2")
+
+    implementation("io.ktor:ktor-server-content-negotiation:3.1.1")
+    implementation("io.ktor:ktor-serialization-jackson:3.1.1")
+
     implementation("com.google.firebase:firebase-admin:9.2.0")
 }
--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -1,13 +1,45 @@
 package com.g5
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.jackson.*
+
+import com.g5.model.IdentifyRequest
+import com.g5.model.Prompt
+import com.g5.model.Response
+
+import com.g5.service.ResponseService
+
 fun Application.configureRouting() {
+    val responseService = ResponseService()
+
+    install(ContentNegotiation) {
+        jackson()
+    }
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/identify-symbol") {
+            try {
+                val requestBody = call.receive<IdentifyRequest>()
+                val processedPrompt = Prompt() // TODO: call prompt service to process input and upload base64 string to bucket
+
+                val response = responseService.submitPrompt(processedPrompt)
+                val generatedResponse = responseService.generateResponse()
+                
+                call.respond(mapOf("symbol" to generatedResponse.getSymbol(), "context" to generatedResponse.getContext()))
+            } catch (ex: Exception) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
