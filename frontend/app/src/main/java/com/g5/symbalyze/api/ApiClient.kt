04932020ecd508526a2ee5e510fdac6841da96a4
milo <106563068+charcoalyy@users.noreply.github.com>
2025-04-04 11:26:11 -0400

    env config for api base url

--- frontend/app/src/main/java/com/g5/symbalyze/api/ApiClient.kt
+++ frontend/app/src/main/java/com/g5/symbalyze/api/ApiClient.kt
@@ -1,11 +1,11 @@
 package com.g5.symbalyze.api
 
+import com.g5.symbalyze.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-// concrete api clients we can call
 object UserApiClient {
-    private const val BASE_URL = "http://172.17.103.131:8080" // NOTE: use your local ip address
+    private const val BASE_URL = BuildConfig.API_BASE_URL // NOTE: please set API_BASE_URL in local.properties
 
     val apiService: UserApiInterface by lazy {
         Retrofit.Builder()
@@ -17,7 +17,7 @@ object UserApiClient {
 }
 
 object SymbolApiClient {
-    private const val BASE_URL = "http://172.17.193.191:8080" // NOTE: use your local ip address
+    private const val BASE_URL = BuildConfig.API_BASE_URL // NOTE: please set API_BASE_URL in local.properties
 
     val apiService: SymbolApiInterface by lazy {
         Retrofit.Builder()
